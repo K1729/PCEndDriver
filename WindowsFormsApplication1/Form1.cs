8c9e7a555e169295713b5a8f68d653a12bb0beeb
LargeMammal <jari.loippo@gmail.com>
2016-08-30 18:36:48 +0300

    Adding method that will enable sending multiple instructions

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -13,30 +13,29 @@ using System.IO;
 using System.IO.Ports;
 using System.Threading;
 
+using WindowsFormsApplication1.Model;
+
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        private readonly object _lock = new object();
+        private readonly Queue<Instruction> _instructions = new Queue<Instruction>();
+        private readonly AutoResetEvent _signal = new AutoResetEvent(false);
+
         public SerialPort ArduinoPort;
         public delegate void passString( string myString );
         public passString myDelegate;
         string message;
         string returnMessage = "";
         const int messageLength = 16;
-        bool done;
 
         public Form1()
         {
             InitializeComponent();
             backgroundWorker1.WorkerSupportsCancellation = true;
-            done = false;
             backgroundWorker1.DoWork += new DoWorkEventHandler(backgroundWorker1_DoWork);
             myDelegate = new passString(infoBoxUpdater);
-        }
-
-        private void infoBoxUpdater(string myString)
-        {
-            InfoBox.AppendText(myString + "\r\n");
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -44,6 +43,34 @@ namespace WindowsFormsApplication1
             InfoBox.Text = "";
             // identify arduino
             thingy();
+        }
+
+        private void Send_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy != true)
+            {
+                _instructions.Clear();
+                message = MessageBox.Text;
+                backgroundWorker1.RunWorkerAsync(COMBox.SelectedItem.ToString());
+            }
+            else
+            {
+                InfoBox.AppendText("Worker is busy...\r\n");
+                InfoBox.AppendText("Leaving instructions in queue.\r\n");
+                Instruction newInstruction = new Instruction(true, message);
+
+                // you need to make sure only
+                // one thread can access the list
+                // at a time
+                lock (_lock)
+                {
+                    _instructions.Enqueue(newInstruction);
+                }
+
+                Thread.Sleep(100);
+                // notify the waiting thread
+                _signal.Set();
+            }
         }
 
         public void thingy()
@@ -112,26 +139,9 @@ namespace WindowsFormsApplication1
             COMBox.Items.AddRange(ports);
         }
 
-        private void Send_Click(object sender, EventArgs e)
+        private void infoBoxUpdater(string myString)
         {
-            if (backgroundWorker1.IsBusy != true)
-            {
-                message = MessageBox.Text;
-                backgroundWorker1.RunWorkerAsync(COMBox.SelectedItem.ToString());
-            }
-            else
-            {
-                InfoBox.AppendText("Worker busy\r\n");
-            }
-        }
-
-        // Stops background worker
-        private void cancelAsyncButton_Click(object sender, EventArgs e)
-        {
-            if (backgroundWorker1.WorkerSupportsCancellation == true)
-            {
-                backgroundWorker1.CancelAsync();
-            }
+            InfoBox.AppendText(myString + "\r\n");
         }
 
         // This is background worker. It does the loop and keeps port alive.
@@ -139,10 +149,11 @@ namespace WindowsFormsApplication1
         {
             BackgroundWorker worker = sender as BackgroundWorker;
             SerialPort sp = new SerialPort((string)e.Argument);
+            Instruction command = null;
 
             string messageArduino = "";
+            bool done = false;
             Sender(sp);
-            Thread.Sleep(200);
 
             while (!done)
             {
@@ -164,18 +175,52 @@ namespace WindowsFormsApplication1
                 }
                 else
                 {
-                    messageArduino += sp.ReadExisting();
-                    Thread.Sleep(1000);
-                    if (InfoBox.InvokeRequired)
+                    if (sp.BytesToRead > 0)
                     {
-                        InfoBox.Invoke(myDelegate, new object[] { ("Return message: " + messageArduino) });
+                        Thread.Sleep(200);
+                        messageArduino += sp.ReadExisting();
+                        if (InfoBox.InvokeRequired)
+                        {
+                            InfoBox.Invoke(myDelegate, new object[] { ("Return message: " + messageArduino) });
+                        }
+                        else
+                        {
+                            InfoBox.Text += "Return message: \r\n";
+                            InfoBox.Text += messageArduino;
+                        }
                     }
+
+                    if (command.newCommand)
+                    {
+                        message = command.ToString();
+                        Sender(sp);
+                        command.newCommand = false;
+                    }
+
+                    else if (_instructions.Count > 0)
+                    {
+                        _signal.WaitOne();
+                        command = null;
+                        lock (_lock)
+                        {
+                            command = _instructions.Dequeue();
+                        }
+                    }
+
                     else
                     {
-                        InfoBox.Text += "Return message: \r\n";
-                        InfoBox.Text += messageArduino;
+                        Thread.Sleep(1000);
                     }
                 }
+            }
+        }
+
+        // Stops background worker
+        private void cancelAsyncButton_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.WorkerSupportsCancellation == true)
+            {
+                backgroundWorker1.CancelAsync();
             }
         }
 
---
+++ WindowsFormsApplication1/Model/Instruction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Model
+{
+    /// <summary>
+    /// Instruction class includes instructions and indicator that tells if said instruction is completed.
+    /// </summary>
+    class Instruction
+    {
+        public bool newCommand { get; set; }
+        private string instructions;
+        
+        public Instruction() { }
+
+        public override string ToString()
+        {
+            return instructions;
+        }
+
+        public Instruction(bool newcommand, string str)
+        {
+            newCommand = newcommand;
+            instructions = str;
+        }
+    }
+}
